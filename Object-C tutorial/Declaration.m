8620e0e5c85111b5aa6ce8f97e9850200bf6fce2
Hengjie Chen <chenhengjie123@gmail.com>
2015-12-04 20:05:49 +0800

    Add example for declaraction

---
+++ Object-C tutorial/Declaration.m
@@ -0,0 +1,34 @@
+//
+//  Declaration.m
+//  Object-C tutorial
+//
+//  Created by Hengjie.chj on 4/12/2015.
+//  Copyright © 2015 Hengjie.chj. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+// Variable declaration:
+extern int a, b;
+extern int c;
+extern float f;
+
+int main ()
+{
+    /* variable definition: */
+    int a, b;
+    int c;
+    float f;
+    
+    /* actual initialization */
+    a = 10;
+    b = 20;
+    
+    c = a + b;
+    NSLog(@"value of c : %d \n", c);
+    
+    f = 70.0/3.0;
+    NSLog(@"value of f : %f \n", f);
+    
+    return 0;
+}
--- Object-C tutorial/helloworld.m
+++ Object-C tutorial/helloworld.m
@@ -23,10 +23,29 @@
 
 @end
 
-int main()
-{
-    /* my first program in Object-c */
-    SampleClass *sampleClass = [[SampleClass alloc]init];
-    [sampleClass sampleMethod];
-    return 0;
-}
+//int main()
+//{
+//    /* my first program in Object-c */
+//    SampleClass *sampleClass = [[SampleClass alloc]init];
+//    [sampleClass sampleMethod];
+//    
+//    /* types */
+//    // Integer types
+//    NSLog(@"Storage size for int : %d \n", sizeof(int));
+//    NSLog(@"Storage size for char : %d \n", sizeof(char));
+//    NSLog(@"Storage size for short : %d \n", sizeof(short));
+//    NSLog(@"Storage size for long : %d \n", sizeof(long));
+//    
+//    
+//    //Floating-Point Types
+//    NSLog(@"Storage size for float : %d \n", sizeof(float));
+//    NSLog(@"Storage size for double : %d \n", sizeof(double));
+//    NSLog(@"Storage size for long double : %d \n", sizeof(long double));
+//    
+//    // void
+//    NSLog(@"Use void in function that have no parameter: int rand(void)");
+//    int rand(void);
+//    int rand();
+//    return 0;
+//    
+//}
